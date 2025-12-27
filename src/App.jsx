e38f0cc1594efd32cad0524944e44b12b8c6367e
hasanMohameth <hasanmohameth@gmail.com>
2025-12-27 21:33:31 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder, {
-  action as createOrderAction,
-} from './features/order/CreateOrder';
+import CreateOrder from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
-import { action as updateOrderAction } from './features/order/UpdateOrder';
+
+import { action as updateOrderAction } from './features/order/UpdateOrder'; 
 
 import AppLayout from './ui/AppLayout';
 
@@ -18,29 +17,31 @@ const router = createBrowserRouter([
     errorElement: <Error />,
 
     children: [
-      {
-        path: '/',
-        element: <Home />,
+
+      { path: '/', 
+        element: <Home /> },
+
+
+      { path: '/menu',
+         element: <Menu />,
+          loader: menuLoader, 
+          errorElement: <Error /> },
+
+
+      { path: '/cart',
+        
+        element: <Cart /> },
+
+
+      { path: '/order/new',
+        
+        element: <CreateOrder /> 
       },
-      {
-        path: '/menu',
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      { path: '/cart', element: <Cart /> },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-        action: createOrderAction,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrderAction,
-      },
+      { path: '/order/:orderId',
+         element: <Order />,
+          loader: orderLoader,
+          action: updateOrderAction,  
+           errorElement: <Error /> },
     ],
   },
 ]);
